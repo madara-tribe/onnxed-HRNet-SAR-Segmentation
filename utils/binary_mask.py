ba38e3d0ddc80f635f16e6934ea93865215281df
萩原辰哉 <hagiharatatsuya@hagiharatatsuyanoMacBook-Air.local>
2021-09-05 18:34:49 +0900

    clean up

--- utils/binary_mask.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-def create_binary_mask(pred, threshold=0.1):
-    markers = np.zeros_like(pred)
-    markers[pred < threshold] = 0
-    markers[pred > threshold] = 1
-    #print(np.unique(markers))
-    return markers
